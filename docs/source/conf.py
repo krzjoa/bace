d8c360b330a4e35fd9f3eb075b569b7b8b270ca0
Krzysztof Joachimiak <kjoa.92@gmail.com>
2018-02-10 20:46:47 +0100

    Algorithms description

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,7 +34,8 @@ import bayes
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 #extensions = ['sphinx.ext.autodoc', 'sphinxcontrib.napoleon', 'sphinx.ext.viewcode', 'sphinx.ext.autosummary']
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.viewcode', 'sphinx.ext.autosummary']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.viewcode',
+              'sphinx.ext.autosummary', 'sphinx.ext.mathbase']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
