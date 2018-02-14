93bffdb0f6cded5d0fa3d3c3e9b90e40fc1c0153
Krzysztof <joachimiak@mywhizzy.com>
2018-02-14 21:03:56 +0100

    bayes

--- bayes/utils.py
+++ bayes/utils.py
@@ -34,13 +34,9 @@ import types
 def inherit_docstring(cls):
     for name, func in vars(cls).items():
         if isinstance(func, types.FunctionType) and not func.__doc__:
-            #print func, 'needs doc'
             for parent in cls.__bases__:
                 parfunc = getattr(parent, name, None)
                 if parfunc and getattr(parfunc, '__doc__', None):
-                    # if parfunc.__name__.starstwith("__"):
-                    #     continue
-                    print parfunc.__name__
                     func.__doc__ = parfunc.__doc__
                     break
     return cls
